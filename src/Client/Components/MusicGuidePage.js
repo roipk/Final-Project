ef14f9100873e9f74b50281e0ea089643f4157ea
Tomer Haliva <halivatomer@gmail.com>
2022-04-30 18:23:41 +0300

    Confirmation Popup

--- src/Client/Components/MusicGuidePage.js
+++ src/Client/Components/MusicGuidePage.js
@@ -3,9 +3,9 @@ import { logOut, loadPage, verifyUser } from "./ManagerComponents";
 import axios from "axios";
 import Select from "react-select";
 import { url } from "./AllPages";
-import collect from "collect.js";
 import SongDebugCard from "./SongDebugCard";
-
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 var currentUser = {};
 
 export default class MusicGuidePage extends Component {
@@ -51,23 +51,33 @@ export default class MusicGuidePage extends Component {
   setPlaylist = (selectedPlaylist) => {
     // console.log(this.state.playlistToView.label.length)
     if (this.state.playlistToView.label.length != 0) {
-      if (
-        confirm(
-          "Are you sure you want to switch playlist? Please make sure you saved your changes, otherwise changes will not take effect"
-        )
-      ) {
-        this.getSongsForDebug(selectedPlaylist.label).then((result) => {
-          this.setState({
-            playlistToView: {
-              value: selectedPlaylist.value,
-              label: selectedPlaylist.label,
+      confirmAlert({
+        title: "Are you sure you want to switch playlist?",
+        message:
+          "Please make sure you saved your changes, otherwise changes will not take effect",
+        buttons: [
+          {
+            label: "Yes",
+            onClick: () => {
+              this.getSongsForDebug(selectedPlaylist.label).then((result) => {
+                this.setState({
+                  playlistToView: {
+                    value: selectedPlaylist.value,
+                    label: selectedPlaylist.label,
+                  },
+                  songs: result,
+                });
+              });
             },
-            songs: result,
-          });
-        });
-      } else {
-        return;
-      }
+          },
+          {
+            label: "No",
+            onClick: () => {
+              return;
+            },
+          },
+        ],
+      });
     } else {
       this.getSongsForDebug(selectedPlaylist.label).then((result) => {
         this.setState({
@@ -138,33 +148,53 @@ export default class MusicGuidePage extends Component {
                 songs={this.state.songs}
               ></SongDebugCard>
             ) : (
-              "Playlit is empty"
+              "Playlist is empty"
             )}
             {/* {this.viewSongs(this.state.songs.length != 0)} */}
             <div className="container-contact100-back-btn">
               <div className="music-guide-wrap-btn" style={{ zIndex: 0 }}>
-              <div className="container-contact100-form-btn">
-                        <div className="wrap-contact100-form-btn">
-                          <div className="research contact100-form-bgbtn"></div>
-                          <button
-                            id="logOut"
-                            type="button"
-                            className="contact100-form-btn"
-                            onClick={() => {
-                              logOut(this.props);
-                            }}
-                          >
-                            <span>
-                              <i
-                                className="fa fa-sign-out fa-lg fa-fw"
-                                aria-hidden="true"
-                                style={{ padding_right: "10px" }}
-                              ></i>
-                              &nbsp;&nbsp;&nbsp;&nbsp; LOG OUT
-                            </span>
-                          </button>
-                        </div>
-                      </div>
+                <div className="container-contact100-form-btn">
+                  <div className="wrap-contact100-form-btn">
+                    <div className="research contact100-form-bgbtn"></div>
+                    <button
+                      id="logOut"
+                      type="button"
+                      className="contact100-form-btn"
+                      onClick={() => {
+                        if (this.state.playlistToView.label.length != 0) {
+                          confirmAlert({
+                            title: "Are you sure you want to logout?",
+                            message:
+                              "Please make sure you saved your changes, otherwise changes will not take effect",
+                            buttons: [
+                              {
+                                label: "Yes",
+                                onClick: () => {
+                                  logOut(this.props);
+                                },
+                              },
+                              {
+                                label: "No",
+                                onClick: () => {
+                                  return;
+                                },
+                              },
+                            ],
+                          });
+                        } else logOut(this.props);
+                      }}
+                    >
+                      <span>
+                        <i
+                          className="fa fa-sign-out fa-lg fa-fw"
+                          aria-hidden="true"
+                          style={{ padding_right: "10px" }}
+                        ></i>
+                        &nbsp;&nbsp;&nbsp;&nbsp; LOG OUT
+                      </span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
